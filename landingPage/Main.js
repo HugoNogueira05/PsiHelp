83b5b551e0fdeb971a1a3f6867279f7416dfac02
HugoNogueira05 <hugonogueira055@gmail.com>
2023-03-14 18:53:14 +0000

    ultima mudança na main page(falta adaptabilidade)

--- landingPage/Main.js
+++ landingPage/Main.js
@@ -9,5 +9,16 @@ const observer =  new IntersectionObserver((entries) => {
     })
 })
 
+const observer2 =  new IntersectionObserver((entries2) => {
+    entries2.forEach((entry2) => {
+        if(entry2.isIntersecting){
+            entry2.target.classList.add('show2')
+        }
+    })
+})
+
 const hiddenElements = document.querySelectorAll('.hidden')
 hiddenElements.forEach((el) => observer.observe(el))
+
+const hiddenElements2 = document.querySelectorAll('.hidden2')
+hiddenElements2.forEach((el2)=> observer2.observe(el2))
