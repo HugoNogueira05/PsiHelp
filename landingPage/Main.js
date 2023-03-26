2948b5b455a79ef2102d21fdc9e26ea52620268e
HugoNogueira05 <hugonogueira055@gmail.com>
2023-03-26 02:23:22 +0100

    adaptabilidade da main page

--- landingPage/Main.js
+++ landingPage/Main.js
@@ -23,3 +23,16 @@ hiddenElements.forEach((el) => observer.observe(el))
 const hiddenElements2 = document.querySelectorAll('.hidden2')
 hiddenElements2.forEach((el2)=> observer2.observe(el2))
 
+const burger = document.getElementById('burger')
+burger.addEventListener("click" ,  function()
+{
+    if(document.querySelector('.navMobile').style.transform == "translateX(0px) scale(1)"){
+    document.querySelector('.navMobile').style.transform = "translateX(100%) scale(0)"
+    console.log("não funciona")
+}
+    else{
+        document.querySelector('.navMobile').style.display = "inline"
+        document.querySelector('.navMobile').style.transform = "translateX(0) scale(1)"
+        console.log("funciona")
+    }
+})
