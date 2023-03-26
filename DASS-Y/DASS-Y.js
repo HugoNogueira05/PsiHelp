ebd579e14d2d77b0bd12d643fc3f73dfcbe6cf83
HugoNogueira05 <hugonogueira055@gmail.com>
2023-03-26 02:49:58 +0100

    alguma adaptabilidade DASS-Y

--- DASS-Y/DASS-Y.js
+++ DASS-Y/DASS-Y.js
@@ -285,3 +285,14 @@ function verificar(){
     function move2(){
         document.getElementById("questao").style.transform = "translateX(-50%) scale(0)"
     }
+    const burger = document.getElementById('burger')
+burger.addEventListener("click" ,  function()
+{
+    if(document.querySelector('.navMobile').style.transform == "translateX(0px) scale(1)"){
+    document.querySelector('.navMobile').style.transform = "translateX(100%) scale(0)"
+}
+    else{
+        document.querySelector('.navMobile').style.display = "inline"
+        document.querySelector('.navMobile').style.transform = "translateX(0) scale(1)"
+    }
+})
