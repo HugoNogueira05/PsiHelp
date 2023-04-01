5d5dc2a2db55e41825b28f81c66155b48a04ea44
HugoNogueira05 <hugonogueira055@gmail.com>
2023-04-01 11:09:22 +0100

    fim da adpatabilidade main

--- landingPage/Main.js
+++ landingPage/Main.js
@@ -28,11 +28,9 @@ burger.addEventListener("click" ,  function()
 {
     if(document.querySelector('.navMobile').style.transform == "translateX(0px) scale(1)"){
     document.querySelector('.navMobile').style.transform = "translateX(100%) scale(0)"
-    console.log("não funciona")
 }
     else{
         document.querySelector('.navMobile').style.display = "inline"
         document.querySelector('.navMobile').style.transform = "translateX(0) scale(1)"
-        console.log("funciona")
     }
 })
